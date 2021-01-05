ad4de12a193c7ea9f1a15d6e4ed120771dc9bae0
Paul Lee <paullee20204@snu.ac.kr>
2021-01-06 08:42:55 +0900

    Use sendgrid for welcome email

--- instaproject/settings/common.py
+++ instaproject/settings/common.py
@@ -28,6 +28,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+ADMINS = [
+    ('Paul Lee', 'paullee20204@gmail.com'),
+]
 
 # Application definition
 
@@ -138,3 +141,14 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 INTERNAL_IPS = ['127.0.0.1']
+
+# Email with Sengdrid
+SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY")
+
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+WELCOME_EMAIL_SENDER = 'paullee20204@gmail.com'
