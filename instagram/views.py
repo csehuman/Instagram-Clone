ccb6186977dde0fcd804d405e7b8dab12623ef94
Paul Lee <paullee20204@snu.ac.kr>
2021-01-08 21:05:29 +0900

    Simple design for timeline

--- instagram/views.py
+++ instagram/views.py
@@ -5,6 +5,12 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .forms import PostForm
 from .models import Tag, Post
 
+@login_required
+def index(request):
+    return render(request, "instagram/index.html", {
+
+    })
+
 
 @login_required
 def post_new(request):
