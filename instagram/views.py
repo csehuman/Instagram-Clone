32c0ef11d28b46d5a2bb90337032936d843086ad
Paul Lee <paullee20204@snu.ac.kr>
2021-01-08 21:46:19 +0900

    Expose following user's post

--- instagram/views.py
+++ instagram/views.py
@@ -1,16 +1,24 @@
 from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import PostForm
 from .models import Tag, Post
 
 @login_required
 def index(request):
+    post_list = Post.objects.all()\
+        .filter(
+        Q(author__in=request.user.following_set.all()) |
+        Q(author=request.user)
+    )
+
     suggested_user_list = get_user_model().objects.all()\
         .exclude(pk=request.user.pk)\
         .exclude(pk__in=request.user.following_set.all())
     return render(request, "instagram/index.html", {
+        'post_list': post_list,
         'suggested_user_list': suggested_user_list,
     })
 
